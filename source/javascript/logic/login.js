ee3936f710301c7b99750bd7c62239a7620c83b1
Arisandi <arisandisatria541@gmail.com>
2023-05-03 15:13:19 +0700

    Fixed loginPage.html and add validation to login page

--- source/javascript/logic/login.js
+++ source/javascript/logic/login.js
@@ -1,8 +1,17 @@
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const loginForm = document.getElementById("login-form");
+const warning = document.getElementById("warning");
 
 const users = localStorage.getItem("users");
+
+const isEmailCorrect = (emailValue, emailData) => {
+  return emailValue == emailData ? true : false;
+};
+
+const isPasswordCorrect = (passwordValue, passwordData) => {
+  return passwordValue == passwordData ? true : false;
+};
 
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -10,8 +19,19 @@ loginForm.addEventListener("submit", (event) => {
   const parsedUsers = JSON.parse(users);
 
   parsedUsers.forEach((user) => {
-    if (email.value == user.emailUser && password.value == user.passwordUser) {
-      console.log("Ok");
+    if (
+      isEmailCorrect(email.value, user.emailUser) &&
+      isPasswordCorrect(password.value, user.passwordUser)
+    ) {
+      warning.classList.add("hidden");
+      warning.classList.remove("block");
+      warning.textContent = "";
+      location.href = "../index.html";
+      console.log("OK");
+    } else {
+      warning.classList.remove("hidden");
+      warning.classList.add("block");
+      warning.textContent = "Invalid email or password! Please check again!";
     }
   });
 });
