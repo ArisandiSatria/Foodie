4e7c3a328c1c14f6acc299b49bd9caf88a4925f0
Arisandi <arisandisatria541@gmail.com>
2023-05-04 14:56:49 +0700

    Fixed bug profile button when in BMI Calc page, cannot go to profile page

--- source/javascript/logic/session.js
+++ source/javascript/logic/session.js
@@ -1,11 +1,21 @@
 // Mendapatkan data session pengguna dari session storage
 let getSession = JSON.parse(localStorage.getItem("sessionData"));
 let access = document.getElementById("access");
-// console.log(getSession.isLoggedIn);
+
+const currentUrl = window.location.href;
+
+const goToProfile = () => {
+  const currentUrl = window.location.href;
+
+  return currentUrl.includes("/page")
+    ? "../page/profile.html"
+    : "/page/profile.html";
+};
+
 // Menggunakan data session pengguna
 if (getSession.isLoggedIn == true) {
   // console.log(`Halo ${getSession.email}, Anda telah login`);
-  access.innerHTML = `<a href="page/profile.html"><button type="button" class="text-white font-medium bg-gradient-to-br from-amber-600 to-yellow-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-1 text-center my-0">
+  access.innerHTML = `<a href="${goToProfile()}"><button type="button" class="text-white font-medium bg-gradient-to-br from-amber-600 to-yellow-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-1 text-center my-0">
                         Profile
                       </button></a>;`;
 }
