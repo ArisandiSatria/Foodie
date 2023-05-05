69e3aaa9470d2310e1d5e4db0e78b8abb39561b7
vian <nalviansend@gmail.com>
2023-05-05 18:24:23 +0700

    fix minor design profile

--- source/javascript/logic/session.js
+++ source/javascript/logic/session.js
@@ -15,7 +15,7 @@ if (getSession == null) {
   access.innerHTML = `<a href="${currentUrl.includes("/page")
     ? "../page/profile.html"
     : "./page/profile.html"
-    }"><button type="button" class="text-white font-medium bg-gradient-to-br from-amber-600 to-yellow-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-1 text-center my-0">
+    }"><button type="button" class="text-white font-semibold bg-yellow-500 hover:bg-yellow-400 0 rounded-lg text-sm px-5 py-1.5 text-center mr-2">
                     Profile
                   </button></a>`;
 }
