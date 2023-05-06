38db20992930fd5ed106940164de14eb65c78f09
vian <nalviansend@gmail.com>
2023-05-06 12:39:22 +0700

    change color button menu

--- source/javascript/loadHeaderFooter.js
+++ source/javascript/loadHeaderFooter.js
@@ -19,7 +19,7 @@ header.innerHTML = `
     <button
       data-collapse-toggle="navbar-default"
       type="button"
-      class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+      class="inline-flex items-center p-2 ml-3 text-sm text-gray-200 hover:text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
       aria-controls="navbar-default"
       aria-expanded="false"
     >
